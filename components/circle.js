1dae212efd95443a045d4803bff7d6e125e210ca
juan <juanjaimes10010@gmail.com>
2025-12-04 16:08:57 -0600

    converting to ecs

---
+++ components/circle.js
@@ -0,0 +1,10 @@
+const circle = () => {
+     this.id = 1;
+     this.collidable = true;
+     this.group = "group";
+};
+
+const game = () => {
+     this.ids = [];
+     this.collision = () => {};
+};
